c0c423aca6065f2c7aad5d642b66b34faea028f4
sangari-suresh <sangarisuresh1993@gmail.com>
2025-01-03 12:52:32 +0530

    created directives component and added functionalities

---
+++ src/app/directives/directives.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+export class DirectivesComponent {
+
+
+  age:number=30;
+  // varaiblename:datatype==intialvale
+
+  ages:number[]=[20,30,40,50,60];
+}
